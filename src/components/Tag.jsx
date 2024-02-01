14a41c9e0aef432cae1a0ea0bc460896d2395193
moonozz <http0127@naver.com>
2024-02-02 00:58:06 +0900

    :lipstick: Style: search 페이지 ui 작업

---
+++ src/components/Tag.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+function Tag(props) {
+  const {
+    lineTxt
+  } = props;
+
+  const getBgClassNm = (lineTxt) => {
+    switch (lineTxt) {
+      case '1':
+        return 'bg-01';
+      case '2':
+        return 'bg-02';
+      case '3':
+        return 'bg-03';
+      case '4':
+        return 'bg-04';
+      case '5':
+        return 'bg-05';
+      case '6':
+        return 'bg-06';
+      case '7':
+        return 'bg-07';
+      case '8':
+        return 'bg-08';
+      case '9':
+        return 'bg-09';
+      case '경강선':
+        return 'bg-gyeonggang';
+      case '경의선':
+        return 'bg-gyeongui';
+      case '경춘선':
+        return 'bg-gyeongchun';
+      case '공항철도':
+        return 'bg-airport';
+      case '김포도시철도':
+        return 'bg-kimpogold';
+      case '서해선':
+        return 'bg-seohae';
+      case '수인분당선':
+        return 'bg-bundang';
+      case '신림선':
+        return 'bg-sillim';
+      case '신분당선':
+        return 'bg-sinbundang';
+      case '용인경전철':
+        return 'bg-everline';
+      case '우이신설경전철':
+        return 'bg-ui_sinseol';
+      case '의정부경전철':
+        return 'bg-uijeongbu';
+      case '인천선':
+        return 'bg-incheon01';
+      case '인천2호선':
+        return 'bg-incheon02';
+      default:
+        return 'gray';
+    }
+  }
+
+  return (
+    // <div className={`${lineTxt === '1' ? 'bg-01' : lineTxt === '2' ? 'bg-02' : lineTxt === '3' ? 'bg-03' : lineTxt === '4' ? 'bg-04' : lineTxt === '5' ? 'bg-05' : lineTxt === '6' ? 'bg-06' : lineTxt === '7' ? 'bg-07' : lineTxt === '8' ? 'bg-08' : lineTxt === '9' ? 'bg-09' : lineTxt === '경강선' ? 'bg-gyeonggang' : lineTxt === '경의선' ? 'bg-gyeongui' : lineTxt === '경춘선' ? 'bg-gyeongchun' : lineTxt === '공항철도' ? 'bg-airport' : lineTxt === '김포도시철도' ? 'bg-kimpogold' : lineTxt === '서해선' ? 'bg-seohae' : lineTxt === '수인분당선' ? 'bg-bundang' : lineTxt === '신림선' ? 'bg-sillim' : lineTxt === '신분당선' ? 'bg-sinbundang' : lineTxt === '용인경전철' ? 'bg-everline' : lineTxt === '우이신설경전철' ? 'bg-ui_sinseol' : lineTxt === '의정부경전철' ? 'bg-uijeongbu' : lineTxt === '인천선' ? 'bg-incheon01' : lineTxt === '인천2호선' ? 'bg-incheon02' : 'gray'}`}>
+    <div className={`${getBgClassNm(lineTxt)} ${lineTxt.length === 1 ? 'px-[10px] py-[4px]' :  'px-[8px] py-[8px]'} rounded-[30px]`}>
+      <span className={`${lineTxt.length === 1 ? 'text-[14px]' : 'text-[10px]'} text-white font-black`} >{lineTxt}</span>
+    </div>
+  )
+}
+
+export default Tag
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import usePooStore from '../store';
+import Tag from '../components/Tag'
 
 function Search() {
   const navi = useNavigate();
@@ -12,7 +13,7 @@ function Search() {
   }, [headers])
 
   return (
-    <div className="h-full pt-[76px] px-[48px] mobile:px-[20px]">
+    <div className="h-full pt-[76px] px-[20px]">
       {searchInputValue.length === 0 ? (
         <div className="flex-center flex-col h-full">
           <img className="w-[120px] h-auto" src={`${process.env.PUBLIC_URL}/assets/symbol-none.png`} alt='우는 poo'/>
@@ -25,20 +26,28 @@ function Search() {
         </div>
       ) : (
         <div className="h-full">
+          <ul className="flex flex-col w-full">
           {searchValue.map((item) => {
             return (
               item.map((i) => {
                 const station = i.station_nm;
-                const line = i.line_num
+                let line = i.line_num
+                if (line[0] === "0") {
+                  line = line.slice(1,2)
+                } 
                 return(
-                  <ul className="flex w-full" key={i.station_cd}>
-                    <li>{line}</li>
-                    <li>{station}</li>
-                  </ul>
-                )
-              })
-            )
-          })}
+                    <li key={i.station_cd}>
+                      <button className='flex w-full py-[8px] mb-[8px] items-center'>
+                        {/* <Tag className={`${line.length === 1 ? 'text-[14px]' : 'text-[9px]' }`} lineTxt={line}>{line}</Tag> */}
+                        <Tag lineTxt={line} />
+                        <span className='text-[14px] ml-[8px]'>{station}</span>
+                      </button>
+                    </li>
+                  )
+                })
+              )
+            })}
+          </ul>
         </div>
       )}
     </div>
