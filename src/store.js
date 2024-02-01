b60e4c9d9dda859d218381bb1ff199c9ea470352
moonozz <http0127@naver.com>
2024-02-01 17:47:35 +0900

    :zap: Feat: 역명 검색 결과 저장과 메모 저장 state 생성

--- src/store.js
+++ src/store.js
@@ -7,10 +7,14 @@ const usePooStore = create((set) => ({
   setHeaders: (text) => set({headers: text}),
   searchInputValue: "",
   setSearchInputValue: (text) => set({searchInputValue: text}),
+  searchValue: [],
+  setSearchValue: (newList) => set({searchValue: [newList]}),
   chooseStation: "",
   setChooseStation: (text) => set({chooseStation: text}),
   chooseStationLine: "",
   setChooseStationLine: (text) => set({chooseStationLine: text}),
+  memo: [],
+  setMemo: (fr_code, text) => set((prev) => ({memo: [...prev.memo, {fr_code: fr_code, text: text}]}))
 }))
 
 export default usePooStore;
