185253709d759d1cf85cf4b1992a60f7c2b9af24
moonozz <http0127@naver.com>
2024-02-10 18:07:50 +0900

    :zap: Perf: 뒤로가기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import usePooStore from '../store'
 import Input from './Input';
-// import station from '../data/seoul-station-info.json';
 import stationJson from '../data/data.json';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Header() {
-  const location = useLocation()
+  const location = useLocation();
+  const navi = useNavigate();
   const { headers, setHeaders, searchInputValue, setSearchInputValue, searchValue, setSearchValue } = usePooStore();
   const [ inputSearch, setInputSearch ] = useState("")
-  // const stationData = station.DATA;
 
   useEffect(() => {
     if(location.pathname === "/") {
@@ -30,21 +29,11 @@ function Header() {
     setInputSearch(e.target.value);
   }
 
-  // const FilterStation = stationData.filter((i) => {
-  //   if (inputSearch.length > 0) {
-  //     return i.station_nm.startsWith(inputSearch);
-  //   }
-  // })
-
   const FilterStationJson = stationJson.filter((i) => {
     if (inputSearch.length > 0) {
       return i.STIN_NM.startsWith(inputSearch);
     }
   })
-
-  // const sortedFilterStation = FilterStation.sort((a, b) => {
-  //   return a.station_nm.localeCompare(b.station_nm)
-  // })
 
   const sortedFilterStation = FilterStationJson.sort((a, b) => {
     return a.STIN_NM.localeCompare(b.STIN_NM)
@@ -53,10 +42,11 @@ function Header() {
   const handleEnterKeypress = (e) => {
     if(e.key === "Enter") {
       e.preventDefault();
-        setSearchInputValue(inputSearch);
-        // console.log(searchInputValue)
-        setSearchValue(sortedFilterStation)
-        // console.log(searchValue)
+      // 검색input에 입력한 텍스트 기억하는 용도
+      setSearchInputValue(inputSearch);
+      // console.log(searchInputValue)
+      setSearchValue(sortedFilterStation)
+      // console.log(searchValue)
     }
   }
 
@@ -67,6 +57,10 @@ function Header() {
     // console.log(searchValue)
   }
 
+  const handleBackPage = () => {
+    navi(-1);
+  }
+
   return (
     <div className='flex w-full h-[76px] max-w-[768px] py-[14px] pr-[20px] fixed z-10 bg-mainBg'>
       {headers === "main" && (
@@ -74,16 +68,19 @@ function Header() {
       )}
       {headers === "search" && (
         <div className='flex w-full'>
-          <button className='flex-center w-[50px] h-[50px]'>
+          <button className='flex-center w-[50px] h-[50px]' onClick={handleBackPage}>
             <img className='' src={`${process.env.PUBLIC_URL}/assets/icon-back.svg`} alt='back-button' />
           </button>
           <Input placeholder='지하철역을 검색하세요.' onChange={handleInput} value={inputSearch} onKeyDown={handleEnterKeypress} onClick={handleSearchValue}/>
         </div>
       )}
       {headers === "result" && (
-        <p>
-          결과페이지 헤더
-        </p>
+        <div className='flex w-full'>
+          <button className='flex-center w-[50px] h-[50px]' onClick={handleBackPage}>
+            <img className='' src={`${process.env.PUBLIC_URL}/assets/icon-back.svg`} alt='back-button' />
+          </button>
+        {/* <Input placeholder='지하철역을 검색하세요.' onChange={handleInput} value={inputSearch} onKeyDown={handleEnterKeypress} onClick={handleSearchValue}/> */}
+      </div>
       )}
     </div>
     
