6213a70a5f35c738f5ae797799bcae8c17036014
moonozz <http0127@naver.com>
2024-02-11 01:57:51 +0900

    :zap: Perf: axios 함수로 빼고 로컬스토리지에 저장해서 사용 + 관련 data 출력 li 만들기

---
+++ src/components/ResultLi.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ResultLi(props) {
+  const {
+    title, contents
+  } = props
+  
+  return (
+    <li className='flex py-[12px]'>
+      <span className='w-[80px] max-w-[80px] text-[14px] text-gray'>{title}</span>
+      <p className='ml-[10px] text-[14px] text-black font-bold'>{contents}</p>
+    </li>
+  )
+}
+
+export default ResultLi
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -2,36 +2,55 @@ import React, { useEffect, useState } from 'react'
 import usePooStore from '../store';
 import Tag from '../components/Tag'
 import axios from 'axios';
+import ResultLi from '../components/ResultLi';
 
 function Result() {
   const { headers, setHeaders, searchValue, chooseStation, chooseStationData, setChooseStationData, sameStation } = usePooStore();
-
   const stationData = chooseStationData.data;
+  const getChooseData = localStorage.getItem("chooseData")
+  const parsedChooseData = JSON.parse(getChooseData);
+
+  const handleFetch = () => {
+    axios
+      .get(`https://openapi.kric.go.kr/openapi/convenientInfo/stationToilet?serviceKey=$2a$10$f${process.env.REACT_APP_TOILET_API_KEY}&format=json&railOprIsttCd=${stationData.RAIL_OPR_ISTT_CD}&lnCd=${stationData.LN_CD}&stinCd=${stationData.STIN_CD}`)
+      .then((res) => {
+        const resData = res.data.body[0]
+        const chooseData = {
+          diapExchNum: resData.diapExchNum, //기저귀교환대개수
+          dtlLoc: resData.dtlLoc, //상세위치
+          exitNo: resData.exitNo, //출구번호
+          gateInotDvNm: resData.gateInotDvNm, //게이트내외구분
+          grndDvNm: resData.grndDvNm, //지상구분
+          lnCd: resData.lnCd, //선코드
+          mlFmlDvNm: resData.mlFmlDvNm, //남녀구분
+          railOprIsttCd: resData.railOprIsttCd, //철도운영기관코드
+          stinCd: resData.stinCd, //역코드
+          stinFlor: resData.stinFlor, //역층
+          toltNum: resData.toltNum //화장실개수
+        }
+        localStorage.setItem("chooseData", JSON.stringify(chooseData));
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
   useEffect(() => {
     setHeaders("result")
     console.log(headers);
     console.log(chooseStationData)
     console.log(sameStation)
-
-    axios
-      .get(`https://openapi.kric.go.kr/openapi/convenientInfo/stationToilet?serviceKey=$2a$10$f${process.env.REACT_APP_TOILET_API_KEY}&format=json&railOprIsttCd=${stationData.RAIL_OPR_ISTT_CD}&lnCd=${stationData.LN_CD}&stinCd=${stationData.STIN_CD}`)
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    
-  }, [headers, chooseStationData, ])
+    handleFetch()
+    console.log(parsedChooseData)
+  }, [headers, chooseStationData])
 
   return (
     <section className="flex flex-col h-full pt-[76px] px-[20px] gap-[20px]">
-      <div className='flex-col text-[20px] mt-[20px] font-black'>
+      <div className='flex-col text-[20px] mt-[20px] font-extrabold'>
         <h2>{stationData.STIN_NM}</h2>
         <h2>화장실 정보</h2>
       </div>
-      <ul className='flex'>
+      <ul className='flex w-full flex-wrap'>
         {
           sameStation.map((i) => {
             return(
@@ -44,6 +63,20 @@ function Result() {
           })
         }
       </ul>
+      <div className='flex flex-col p-[20px] bg-white rounded-[16px]'>
+        <img src={`${process.env.PUBLIC_URL}/assets/symbol.png`} alt='poopoo symbol' className='w-[50px] mb-[12px]'/>
+        <ul className='flex flex-col'>
+          <ResultLi title={`지상 지하`} contents={`${parsedChooseData.grndDvNm}`} />
+          {parsedChooseData.gateInotDvNm !== "안" ? 
+            <ResultLi title={`게이트 밖/안`} contents={"카드찍고 나가야해요😭"} />
+          : 
+            <ResultLi title={`게이트 밖/안`} contents={"카드 찍을 필요 없어요!🥳"} />
+
+          }
+          <ResultLi title={`출구번호`} contents={`${parsedChooseData.exitNo}번 출구에서 가까워요`} />
+          <ResultLi title={`상세 위치`} contents={`${parsedChooseData.dtlLoc}`} />
+        </ul>
+      </div>
     </section>
   )
 }
