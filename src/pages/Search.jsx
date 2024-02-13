c37080a8e52c7b21be96b1d5f445addc4938966f
moonozz <http0127@naver.com>
2024-02-13 15:02:06 +0900

    :lipstick: Style: search페이지 스타일 수정

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -34,12 +34,12 @@ function Search() {
       {searchInputValue.length === 0 ? (
         <div className="flex-center flex-col h-full">
           <img className="w-[120px] h-auto" src={`${process.env.PUBLIC_URL}/assets/symbol-none.png`} alt='우는 poo'/>
-          <p className="mt-[12px] text-[14px]">검색어를 입력해주세요.</p>
+          <p className="mt-[12px] text-[14px] text-gray">검색어를 입력해주세요.</p>
         </div>
       ) : searchValue.length === 0 ? (
         <div className="flex-center flex-col h-full">
           <img className="w-[120px] h-auto" src={`${process.env.PUBLIC_URL}/assets/symbol-none.png`} alt='우는 poo'/>
-          <p className="mt-[12px] text-[14px]">검색 내역이 없습니다.</p>
+          <p className="mt-[12px] text-[14px] text-gray">검색 내역이 없습니다.</p>
         </div>
       ) : (
         <div className="h-full">
